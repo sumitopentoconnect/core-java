2d8afbc9b27c091a98b5c4ab3010eb75941a728e
Sumit Kumar Sharma <sumit4myself@gmail.com>
2024-12-25 14:59:07 +0530

    common files added

--- java10/src/test/java/com/example/corejava/CoreJavaApplicationTests.java
+++ java10/src/test/java/com/example/corejava/CoreJavaApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.corejava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class CoreJavaApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
--- java9/src/test/java/com/example/corejava/CoreJavaApplicationTests.java
+++ java9/src/test/java/com/example/corejava/CoreJavaApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.corejava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CoreJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
